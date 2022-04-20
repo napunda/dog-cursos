935e13bd3526c2b824223b195bc7e93aa712712c
napunda <andersondroide14@gmail.com>
2022-04-20 17:04:23 -0400

    20/04

--- views/cursos.php
+++ views/cursos.php
@@ -1,12 +1,17 @@
+<?php include 'db.php';?>
+
 <div class="container">
-    <div class="owl-carousel">
-        <div> Your Content </div>
-        <div> Your Content </div>
-        <div> Your Content </div>
-        <div> Your Content </div>
-        <div> Your Content </div>
-        <div> Your Content </div>
-        <div> Your Content </div>
-    </div>
+    <?php 
+		while($linha = mysqli_fetch_array($consulta_cursos)){
+			echo '<div class="curso">';
+                echo '<a href="?pagina=cursos"><img src='.$linha['img'].'/></a>';
+                echo $linha['name'];
+			    // echo $linha['Duração'];
+            echo '</div>';
+		?>
+
+    <?php
+			}
+		?>
 
 </div>
